118df35282f0e1066c903de64ca0c6ea070c2b75
Jason Hao <jyhao1998@gmail.com>
2024-02-18 21:35:17 -0800

    build: add spring-boot-starter-data-jpa

---
+++ src/main/java/com/jyhaoo/tennisassociationsystem/domain/entities/PlayerEntity.java
@@ -0,0 +1,2 @@
+package com.jyhaoo.tennisassociationsystem.domain.entities;public class PlayerEntity {
+}
---
+++ src/main/java/com/jyhaoo/tennisassociationsystem/repositories/PlayerRepository.java
@@ -0,0 +1,2 @@
+package com.jyhaoo.tennisassociationsystem.repositories;public interface PlayerRepository {
+}
