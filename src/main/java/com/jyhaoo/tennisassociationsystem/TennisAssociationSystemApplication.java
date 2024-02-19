e30be95015b814cf9b9b8eec0a9acdfcbd2a24ae
Jason Hao <jyhao1998@gmail.com>
2024-02-18 21:27:58 -0800

    Initial commit

---
+++ src/main/java/com/jyhaoo/tennisassociationsystem/TennisAssociationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.jyhaoo.tennisassociationsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TennisAssociationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TennisAssociationSystemApplication.class, args);
+	}
+
+}
