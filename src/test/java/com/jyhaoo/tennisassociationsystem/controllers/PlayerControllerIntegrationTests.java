b02639317671d5bea66499bd0be6f7cc9812e72b
Jason Hao <jyhao1998@gmail.com>
2024-02-20 13:44:06 -0800

    refactor: removed unused imports

--- src/test/java/com/jyhaoo/tennisassociationsystem/controllers/PlayerControllerIntegrationTests.java
+++ src/test/java/com/jyhaoo/tennisassociationsystem/controllers/PlayerControllerIntegrationTests.java
@@ -9,7 +9,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
